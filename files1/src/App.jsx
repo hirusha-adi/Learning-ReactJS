82b0c3538ce210edfc6088630e2109e41dbef573
Hirusha Adikari <hirushaadi@gmail.com>
2024-10-31 23:09:30 -0400

    stuff

--- files1/src/App.jsx
+++ files1/src/App.jsx
@@ -1,12 +1,14 @@
 import { Header } from './components/Header.jsx';
 import { Footer } from './components/Footer.jsx'
 import { TaskList } from './components/TaskList.jsx';
+import { InputForm } from './components/InputForm.jsx';
 
 function App() {
     return (
         <>
             <Header />
             <TaskList pgTitle="ToDo Tasks" />
+            <InputForm />
             <Footer />
         </>
     )
---
+++ files1/src/components/InputForm.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+
+const InputForm = () => {
+
+    const [taskValue, setTaskValue] = useState("");
+
+    const handleChange = (event) => {
+        setTaskValue(event.target.value)
+    }
+
+    const handleReset = () => {
+        setTaskValue("")
+    }
+
+    return (
+        <>
+            <form>
+                <input type="text" onChange={handleChange} placeholder="task name" autoComplete="off" value={taskValue} />
+                <button type="submit">Add Task</button>
+                <span onClick={handleReset} className="reset">Reset</span>
+            </form>
+        </>
+    )
+}
+
+export { InputForm }
