51bc03f2a94e208110d0272e2ad571f3a76b15a3
hirusha-adi <hirushaadi@gmail.com>
2024-11-05 22:06:58 +0530

    nav on click

--- files4/src/components/Contact.jsx
+++ files4/src/components/Contact.jsx
@@ -1,7 +1,17 @@
+import { useNavigate } from "react-router-dom"
+
 const Contact = () => {
+
+    const navigate = useNavigate()
+
+    const handleSubmit = () => {
+        return navigate("/")
+    }
     return (
         <>
             Contact
+            <br />
+            <button onClick={handleSubmit}>Submit Form</button>
         </>
     )
 }
