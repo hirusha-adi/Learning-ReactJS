6d186ef7aa0ba5b283063ad98cf735d3f5d83b2e
hirusha-adi <hirushaadi@gmail.com>
2024-11-11 07:10:48 +0530

    make it responsive

--- files5/src/components/Header.jsx
+++ files5/src/components/Header.jsx
@@ -3,16 +3,16 @@ const Header = () => {
 
     return (
         <div className="max-w-7xl m-auto">
-            <header className="h-16 border-b" >
-                <div className="flex justify-between items-center py-2 mx-2">
+            <header className="h-28 sm:h-16 border-b" >
+                <div className="flex flex-col sm:flex-row justify-between items-center py-2 mx-2">
                     <div className="flex items-center" >
                         <img className="w-12" src="https://placehold.co/100x100" alt="Logo" />
                         <span className="ml-2 text-2xl">My App</span>
                     </div>
-                    <nav>
-                        <span className="p-3 text-lg hover:bg-gray-100 hover:cursor-pointer hover: rounded">Home</span>
-                        <span className="p-3 text-lg hover:bg-gray-100 hover:cursor-pointer hover: rounded">Products</span>
-                        <span className="p-3 text-lg hover:bg-gray-100 hover:cursor-pointer hover: rounded">About Us</span>
+                    <nav className="max-sm:my-3">
+                        <span className="p-2 text-lg hover:bg-gray-100 hover:cursor-pointer hover: rounded">Home</span>
+                        <span className="p-2 text-lg hover:bg-gray-100 hover:cursor-pointer hover: rounded">Products</span>
+                        <span className="p-2 text-lg hover:bg-gray-100 hover:cursor-pointer hover: rounded">About Us</span>
                     </nav>
                 </div>
             </header>
--- files5/src/components/Hero.jsx
+++ files5/src/components/Hero.jsx
@@ -3,8 +3,8 @@ const Hero = () => {
 
     return (
         <section className="text-center my-20">
-            <h1 className="text-4xl">Lorem ipsum dolor sit.</h1>
-            <p className="text-xl mt-2 mb-7">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, nam.</p>
+            <h1 className="text-4xl max-sm:text-3xl">Lorem ipsum dolor sit.</h1>
+            <p className="text-xl mt-2 mb-7 max-sm:text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, nam.</p>
             <button className="text-lg border p-3 rounded-2xl bg-blue-700 text-slate-50 hover:bg-blue-900">Learn More</button>
         </section>
     )
