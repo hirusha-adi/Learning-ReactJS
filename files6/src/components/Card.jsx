c5b3c0f6eb9ab059e64abcee7273589cd14f3b1c
hirusha-adi <hirushaadi@gmail.com>
2024-11-11 18:07:35 +0530

    fix size

--- files6/src/components/Card.jsx
+++ files6/src/components/Card.jsx
@@ -1,17 +1,23 @@
 
-const Card = () => {
+const Card = ({ movie }) => {
     return (
         <>
-            <div className="max-w-sm border rounded-2xl p-3 hover:cursor-pointer m-2">
-                <img className="rounded" src="https://placehold.co/500x600" alt="Product" />
-                <h1 className="text-2xl mb-3 mt-5">Sony 123</h1>
-                <p className="pt-2 border-t">Lorem ipsum dolor sit amet consectetur adipisicing elit. Error dolore quis ratione iste!</p>
-                <button
-                    type="button"
-                    className="inline-flex items-center px-3 py-1.5 mt-5 mb-2 border border-transparent text-xs font-medium rounded-xl shadow-sm text-white bg-gray-500 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Read More
-                </button>
+            <div className="max-w-72 border rounded-2xl p-3 hover:cursor-pointer m-2">
+                <img className="rounded-lg" src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt="Product" />
+                <h1 className="text-xl mb-3 mt-5 pb-2 border-b font-bold">
+                    {movie.original_title}
+                </h1>
+                <div >
+                    {movie.adult &&
+                        <div className="inline-block bg-red-600 text-white rounded-full px-3 py-1 text-sm font-bold">
+                            18+
+                        </div>
+                    }
+                </div>
+                <p>
+                    {`${movie.overview.substring(0, 120)}${movie.overview.length > 120 ? '...' : ''}`}
+                </p>
+
             </div>
         </>
     );
