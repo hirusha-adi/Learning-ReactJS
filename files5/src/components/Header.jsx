c3af53c11251e4f460f13906430105e8a4de7969
hirusha-adi <hirushaadi@gmail.com>
2024-11-11 06:22:48 +0530

    basic tailwind project

---
+++ files5/src/components/Header.jsx
@@ -0,0 +1,13 @@
+
+const Header = () => {
+
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+        </>
+    )
+}
+
+export { Header }
