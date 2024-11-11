ae2eb4b73169ec809d78202c5525f620b936c9e7
hirusha-adi <hirushaadi@gmail.com>
2024-11-11 19:31:35 +0530

    stuff

---
+++ files6/src/hooks/useFetch.jsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (api) => {
+    const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${api}?language=en-US&page=1`
+
+    useEffect(() => {
+        async function fetchMovies() {
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjQ5N2VmMjU5NDdmNGJjZTMyZDZmOWY1MjQ3NzU1NCIsIm5iZiI6MTczMTMyMTcyMi4zMzM0MDYyLCJzdWIiOiI2NzMxZGM2YjY4OTVmMzgyMmU0NWY2YWEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.aSmki4Z2_wrkDc_8O8ayf_H41imRIon-6oTmyvl-gDY'
+                }
+            };
+            const response = await fetch(url, options)
+            const data = await response.json()
+            setData(data.results)
+        }
+        fetchMovies()
+    }, [api])
+
+    return { data }
+}
